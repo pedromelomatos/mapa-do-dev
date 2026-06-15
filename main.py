d8dff1fca1d4c50fb80a228fd5642ed75ec44514
Pedro Henrique Melo Matos <melomatosp@gmail.com>
2026-06-15 18:40:32 -0300

    feat: setup SQLAlchemy

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from flask import Flask, url_for, render_template
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
 app =  Flask(__name__)
+
+load_dotenv()
+
+user = os.getenv("user")
+senha = os.getenv("senha")
+host = os.getenv("host")
+nome_do_banco = os.getenv("nome_do_banco")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql+psycopg2://{user}:{senha}@{host}/{nome_do_banco}"
+
+
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
